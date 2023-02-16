7f4bbcb0ca1acb633ec179329d68450f754ce300
nozerx <nozerdev@gmail.com>
2023-02-16 22:09:14 +0530

    basic node to node connectivity achieved
    nodes are able to connect to each other over internet.
    
    Result verified using my mobile to pc connection, where mobile was connected to the mobile network and pc was connected to wifi.

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"peer-to-peer-app-v0.01/p2pnet"
+	"peer-to-peer-app-v0.01/pubsub"
 )
 
+const topic = "rex/filegroup/1"
+const service = "rex/service/test"
+
 func main() {
-	p2pnet.EstablishP2P()
+	ctx, host := p2pnet.EstablishP2P()
+	kad_dht := p2pnet.HandleDHT(ctx, host)
+	pubsub.HandlePubSub(ctx, host, topic)
+	go p2pnet.DiscoverPeers(ctx, host, kad_dht, service)
 	var i = 0
 	for i = 10; ; i++ {
 	}
